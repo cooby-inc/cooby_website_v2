f96dccb8921219282d51fe27c36b3ac3ac6adace
leokuo0724 <p900372012@gmail.com>
2021-06-26 13:16:06 +0800

    add redirect function

---
+++ src/assets/js/redirect.js
@@ -0,0 +1,10 @@
+$(() => {
+  console.log(window.location.pathname)
+  if (window.location.pathname === '/pricing.html' ||
+      window.location.pathname === '/pricing' ||
+      window.location.pathname === '/zh-tw/pricing.html' ||
+      window.location.pathname === '/zh-tw/pricing'
+    ) {
+    window.location.pathname = '/'
+  }
+})
