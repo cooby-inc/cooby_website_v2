e3e77f6f74226b76d68cf3a404803da29ea78c97
leokuo0724 <p900372012@gmail.com>
2021-06-29 21:04:51 +0800

    feat: add api connection

--- src/assets/js/redeem.js
+++ src/assets/js/redeem.js
@@ -1,26 +1,82 @@
 $('#redemption_form').on('submit', function(e) {
   e.preventDefault()
   
+  setActivateAccountLoading(true)
+
   var inputs = $('#redemption_form :input')
   var data = {}
   inputs.each(function() {
     data[this.name] = $(this).val().trim()
   })
-  console.log(data)
 
   // check password
   if (data.password.length < 8 || data.confirmPassword.length < 8) {
     showErrorModalWithMsg('Password should be minimum 8 characters')
+    setActivateAccountLoading(false)
     return
   }
   if (data.password !== data.confirmPassword) {
     showErrorModalWithMsg('Passwords do not match')
+    setActivateAccountLoading(false)
     return
   }
-  
+
+  // sign up user
+  // production: 'https://cooby-taroko.herokuapp.com'
+  // staging: 'https://api-staging.cooby.co'
+  var apiURL = 'https://api-staging.cooby.co'
+
+  $.ajax({
+    type: 'POST',
+    url: apiURL + '/users',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify(data),
+    success: function(res) {
+      var authToken = 'Bearer ' + res.token
+      $.ajax({
+        type: 'PUT',
+        url: apiURL + '/info',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          lang: 'en',
+          name: data.name,
+          company: data.company,
+          timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        }),
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader('Authorization', authToken)
+        },
+        success: function(res) {
+          setActivateAccountLoading(false)
+          window.location.pathname = '/redeem-success.html'
+          window.location.search = '?email=' + data.email
+        },
+        error: function(res) {
+          setActivateAccountLoading(false)
+          showErrorModalWithMsg(res)    
+        }
+      })
+    },
+    error: function(res) {
+      setActivateAccountLoading(false)
+      showErrorModalWithMsg(res.responseJSON.error_message)
+    }
+  })
+
 })
 
 function showErrorModalWithMsg(msg) {
   $('#errorModal').modal('show')
   $('#errorModal #errorMsg').text(msg)
+}
+
+function setActivateAccountLoading(bool) {
+  if (bool) {
+    $('#redemption_form #redeem_button span').show()
+    $('#redemption_form #redeem_button p').hide()
+  } else {
+    $('#redemption_form #redeem_button span').hide()
+    $('#redemption_form #redeem_button p').show()
+  }
 }
