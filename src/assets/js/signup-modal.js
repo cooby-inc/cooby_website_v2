f16dc99f206cbc216660f583f6cfbb61a7277211
leokuo0724 <p900372012@gmail.com>
2021-06-30 21:02:26 +0800

    restore post function

--- src/assets/js/signup-modal.js
+++ src/assets/js/signup-modal.js
@@ -1,19 +1,17 @@
 $('#signup_form').submit(function(e){
   e.preventDefault();
   //do some verification
-  console.log(data)
-
-  // $.post({
-  //   url: "https://api.cooby.co/leads",
-  //   data: $(this).serialize(),
-  //   success: function(data)
-  //   {
-  //     console.log("success");
-  //     $('#modalSignupHorizontal').modal('hide');
-  //     // $('#scheduleModal').modal('show');
-  //     $('#downloadAndScheduleModal').modal('show');
-  //   }
-  // });
+  $.post({
+    url: "https://api.cooby.co/leads",
+    data: $(this).serialize(),
+    success: function(data)
+    {
+      console.log("success");
+      $('#modalSignupHorizontal').modal('hide');
+      // $('#scheduleModal').modal('show');
+      $('#downloadAndScheduleModal').modal('show');
+    }
+  });
 });
 
 $('#scheduleModalBtn').click(function(e) {
