bd68bc3915d2f113fe4e4a627313b3566438bb50
Leo Kuo <40346834+leokuo0724@users.noreply.github.com>
2021-07-07 17:06:10 +0800

    feat: add chinese version blog (#51)

--- src/assets/js/blog.js
+++ src/assets/js/blog.js
@@ -1,9 +1,22 @@
 $(function() {
-  if (window.location.pathname !== '/blog.html' && window.location.pathname !== '/blog') {
+  if (window.location.pathname !== '/blog.html'
+    && window.location.pathname !== '/blog'
+    && window.location.pathname !== '/zh-tw/blog.html'
+    && window.location.pathname !== '/zh-tw/blog') {
     return
   }
+
   $.getJSON(window.location.origin + '/articles.json', function(json) {
-    var data = json.en
+    var data
+    if (window.location.pathname === '/blog.html' || window.location.pathname === '/blog') {
+      data = json.en
+    } else if (window.location.pathname === '/zh-tw/blog.html' || window.location.pathname === '/zh-tw/blog') {
+      data = json.zh
+    } else {
+      console.log('wrong path')
+      return
+    }
+    
     var container = $('#horizontal-article-container')
     for(var i=0; i<data.length; i++) {
       generateAndAppendArticleCard(container, data[i])
@@ -28,10 +41,10 @@ function generateAndAppendArticleCard(parent, data) {
   var badgeSpan = $('<span>', {class: 'badge badge-pill badge-gray-600 badge-float badge-float-outside'}).append(badgeSpanSpan)
   var badge = $('<div>', {class: 'col-12'}).append(badgeSpan)
   // card's children 2
-  var imageImg = $('<img>', {class: 'img-fluid d-md-none invisible', src: data.backgroundImage})
+  var imageImg = $('<img>', {class: 'img-fluid d-md-none invisible', src: data.articleImage})
   // var test = require('assets/img/shapes/curves/curve-4.svg')
   var imageShape = $('<div>', {class: 'shape shape-right shape-fluid-y svg-shim text-white d-none d-md-block'}).load('assets/img/shapes/curves/curve-4.svg')
-  var image = $('<a>', {class: 'col-12 col-md-6 bg-cover card-img-left', href: data.url}).css('background-image', 'url('+ data.backgroundImage +')')
+  var image = $('<a>', {class: 'col-12 col-md-6 bg-cover card-img-left', href: data.url}).css('background-image', 'url('+ data.articleImage +')')
   image.append([imageImg, imageShape])
   // card's children 3
   var contentBodyTitle = $('<h3>').text(data.title)
