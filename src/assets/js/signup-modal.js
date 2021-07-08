18b3cb4eff6e6008c6d3d97b40e66f43fc674f15
Leo Kuo <40346834+leokuo0724@users.noreply.github.com>
2021-07-08 12:08:50 +0800

    feat: add country code field (#52)
    * feat: add country field
    
    * fix: use form data

--- src/assets/js/signup-modal.js
+++ src/assets/js/signup-modal.js
@@ -1,9 +1,23 @@
 $('#signup_form').submit(function(e){
   e.preventDefault();
+  var inputData = {}
+  $('#signup_form :input').each(function(input) {
+    inputData[this.name] = $(this).val().trim()
+  })
+  var outputData = new FormData()
+  outputData.append('name', inputData.firstName + ' ' + inputData.lastName)
+  outputData.append('company', inputData.company)
+  outputData.append('email', inputData.email)
+  outputData.append('title', inputData.title)
+  outputData.append('motivation', inputData.motivation)
+  outputData.append('phone', inputData.countryCode + ' ' + inputData.phone)
+
   //do some verification
   $.post({
     url: "https://api.cooby.co/leads",
-    data: $(this).serialize(),
+    data: outputData,
+    processData: false,
+    contentType: false,
     success: function(data)
     {
       console.log("success");
@@ -26,10 +40,18 @@ $('#downloadAndScheduleModalBtn').click(function(e) {
 
 
 $(document).ready(function() {
-
+  
   if(window.location.href.indexOf('#signupModal') != -1) {
     $('#modalSignupHorizontal').modal('show');
   }
+
+  $.getJSON(window.location.origin + '/country-codes.json', function(json) {
+    var container = $('#country-code-selector')
+    for(var i=0; i<json.length-1; i++) {
+      var option = $('<option>', {value: json[i].dial_code}).text(json[i].name + ' ' + json[i].dial_code)
+      container.append(option)
+    }
+  })
 
 });
 
