b3371d866b62da3b626d8ad3822f6a352d14ec8f
leokuo0724 <p900372012@gmail.com>
2021-07-09 15:16:18 +0800

    feat: add login-canny.js

--- src/assets/js/login-canny.js
+++ src/assets/js/login-canny.js
@@ -1,32 +1,137 @@
-
 $(function() {
-  switchToLogInForm(true)
-})
-
-$('#canny-login-form').on('submit', function(e) {
-  e.preventDefault()
-  console.log('submit')
-})
-
-$('#canny-signup-form').on('submit', function(e) {
-  e.preventDefault()
-  console.log('submit')
-})
-
-$('#canny-login-form #canny-signup-switch').on('click', function() {
-  switchToLogInForm(false)
-})
-
-$('#canny-signup-form #canny-login-switch').on('click', function() {
-  switchToLogInForm(true)
-})
-
-function switchToLogInForm(isLogIn) {
-  if (isLogIn) {
-    $('#canny-login-form').show()
-    $('#canny-signup-form').hide()
-  } else {
-    $('#canny-login-form').hide()
-    $('#canny-signup-form').show()
+  if (window.location.pathname !== '/login-canny.html' && window.location.pathname !== '/login-canny') {
+    return
   }
-}
+
+  switchToLogInForm(true)
+
+  $('#canny-login-form #canny-signup-switch').on('click', function() {
+    switchToLogInForm(false)
+  })
+  $('#canny-signup-form #canny-login-switch').on('click', function() {
+    switchToLogInForm(true)
+  })
+
+  var apiURL = 'https://api-staging.cooby.co'
+  var loginForm = $('#canny-login-form')
+  loginForm.on('submit', function(e) {
+    e.preventDefault()
+    setLoading(true, loginForm)
+
+    var data = generateUserData(loginForm)
+    var isLegalPassword = checkPassword(data, false)
+    if (!isLegalPassword) { 
+      setLoading(false, loginForm)
+      return
+    }
+
+    $.ajax({
+      type: 'POST',
+      url: apiURL + '/login',
+      contentType: 'application/json',
+      crossDomain: true,
+      data: JSON.stringify(data),
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Cooby-App-Platform', 'Canny')
+      },
+      success: function(res) {
+        console.log('success')
+        setLoading(false, loginForm)
+      },
+      error: function(res) {
+        console.log('failed')
+        setLoading(false, loginForm)
+        showErrorModalWithMsg(res.responseJSON.error_message)
+      }
+    })
+  })
+  var signupForm = $('#canny-signup-form')
+  signupForm.on('submit', function(e) {
+    e.preventDefault()
+    setLoading(true, signupForm)
+
+    var data = generateUserData(signupForm)
+    var isLegalPassword = checkPassword(data, true)
+    if (!isLegalPassword) {
+      setLoading(false, signupForm)
+      return
+    }
+
+    // create user api
+  })
+
+
+  // functions
+  function checkPassword(data, hasConfirmPassword) {
+    if (data.password.length < 8 || (hasConfirmPassword && data.confirmPassword.length < 8)) {
+      showErrorModalWithMsg('Password should be minimum 8 characters')
+      return false
+    }
+    if (hasConfirmPassword && (data.password !== data.confirmPassword)) {
+      showErrorModalWithMsg('Passwords do not match')
+      return false
+    }
+    return true
+  }
+
+  function generateUserData(form) {
+    var result = {}
+    form.serializeArray().forEach(function(input) {
+      result[input.name] = input.value
+    })
+    return result
+  }
+
+  function setLoading(bool, target) {
+    if (bool) {
+      target.find('.canny-cta-button').prop('disabled', true)
+      target.find('.canny-cta-button span').show()
+      target.find('.canny-cta-button p').hide()
+    } else {
+      target.find('.canny-cta-button').prop('disabled', false)
+      target.find('.canny-cta-button span').hide()
+      target.find('.canny-cta-button p').show()
+    }
+  }
+
+  function switchToLogInForm(isLogIn) {
+    if (isLogIn) {
+      $('#canny-login-form').show()
+      $('#canny-signup-form').hide()
+    } else {
+      $('#canny-login-form').hide()
+      $('#canny-signup-form').show()
+    }
+  }
+
+  function showErrorModalWithMsg(msg) {
+    $('#errorModal').modal('show')
+    $('#errorModal #errorMsg').text(msg)
+  }
+  
+  // function getQueryParameterByName(name) {
+  //   var pairStrings = window.location.search.slice(1).split('&');
+  //   var pairs = pairStrings.map(function(pair) {
+  //     return pair.split('=');
+  //   });
+  //   return pairs.reduce(function(value, pair) {
+  //     if (value) return value;
+  //     return pair[0] === name ? decodeURIComponent(pair[1]) : null;
+  //   }, null);
+  // }
+  function getRedirectURL(ssoToken) {
+    // var redirectURL = getQueryParameterByName('redirect')
+    var redirectURL = 'https://cooby.co/login-canny.html'
+    // var companyID = getQueryParameterByName('companyID')
+    var companyID = '60e6bb7514f82a74118c91da'
+    if (redirectURL.indexOf('https://') !== 0 || !companyID) {
+      return null;
+    }
+    return 'https://canny.io/api/redirects/sso?companyID=' + companyID + '&ssoToken=' + ssoToken + '&redirect=' + redirectURL;
+  }
+})
+
+// var redirectURL = getRedirectURL(ssoToken);
+// if (redirectURL) {
+//   window.location.assign(redirectURL);
+// }
